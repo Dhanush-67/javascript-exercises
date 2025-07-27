8c65cf3c89cd7c48484681a7f0c9fa3870b5b8cf
Zeeton <146897841+Dhanush-67@users.noreply.github.com>
2025-07-27 00:45:53 -0400

    Finished exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, num) {
+    let total = 0;
+    if(start > num){
+        let x = start;
+        start = num;
+        num = x;
+    }
+    if(start < 0 || num <0){
+        return "ERROR";
+    }
+    if (!Number.isInteger(start) || !Number.isInteger(num)) {
+        return "ERROR";
+    }
+    for(let i = start; i <= num; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
