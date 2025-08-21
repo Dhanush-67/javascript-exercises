ab0b3f96220ed5468b787eaa2495bcff2857a0a6
Zeeton <146897841+Dhanush-67@users.noreply.github.com>
2025-08-21 16:43:25 -0400

    Finished exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let copy = str.replace(/[^\w]|_/g, "").toLowerCase();
+  let reverse_str = copy.split("").reverse().join("");
+  return reverse_str == copy;
 };
 
 // Do not edit below this line
