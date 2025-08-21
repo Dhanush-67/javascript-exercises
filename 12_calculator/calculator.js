5efe1825776787fd4808776bceaee5961e2c4dbd
Zeeton <146897841+Dhanush-67@users.noreply.github.com>
2025-08-21 14:52:00 -0400

    finished the exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((product, num) => product * num, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let i = n;
+  let result = 1;
+  while (i > 1) {
+    result *= i;
+    i--;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +35,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
