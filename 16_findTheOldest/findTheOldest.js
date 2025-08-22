c2449c4a90a7692165d6311334f3d0fb54a58d63
Zeeton <146897841+Dhanush-67@users.noreply.github.com>
2025-08-22 17:08:58 -0400

    FInished exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldest = 0;
+  let name;
+  for (let person of people) {
+    if (person.yearOfDeath - person.yearOfBirth >= oldest) {
+      oldest = person.yearOfDeath - person.yearOfBirth;
+      name = person;
+    }
+  }
+  return name;
 
+  const findTheOldest = function (people) {
+    return people.reduce((oldest, person) => {
+      // If yearOfDeath is missing, use the current year
+      const currentYear = new Date().getFullYear();
+      const age = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+      const oldestAge =
+        (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+      return age > oldestAge ? person : oldest;
+    });
+  };
 };
 
 // Do not edit below this line
